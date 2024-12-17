1347565b1c4b214913a3203f1e5d18fb42adb23b
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-17 16:45:57 -0300

    feat: add JFileChooser function to open system file dialog

--- src/main/java/org/william/eva/ui/Frame.java
+++ src/main/java/org/william/eva/ui/Frame.java
@@ -1,15 +1,24 @@
 package org.william.eva.ui;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
 import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 public class Frame {
-	private int WIDTH = 300;
-	private int HEIGHT = 300;
+	private final int WIDTH = 300;
+	private final int HEIGHT = 300;
 	private JFrame frame;
 
 	public Frame() {
@@ -17,7 +26,10 @@ public class Frame {
 	}
 
 	private void initialize() {
+		JFileChooser jFile = new JFileChooser("c:");
+		
 		FlatDarkLaf.setup();
+		FlatLightLaf.setup();
 		
 		try {
 			UIManager.setLookAndFeel(new FlatDarkLaf());
@@ -32,6 +44,25 @@ public class Frame {
 					frame.setSize(WIDTH, HEIGHT);
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					frame.setLocationRelativeTo(null);
+					
+					JMenuBar menuBar = new JMenuBar();
+					frame.setJMenuBar(menuBar);
+					
+					JMenu mnNewMenu = new JMenu("File");
+					menuBar.add(mnNewMenu);
+					
+					JMenuItem mntmNewMenuItem = new JMenuItem("Open");
+					mntmNewMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
+					mntmNewMenuItem.addActionListener(new ActionListener() {
+
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							jFile.showOpenDialog(null);
+						}
+						
+					});
+					mnNewMenu.add(mntmNewMenuItem);
+					
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
