8f66de98184a4d076b882b8c4108d7820f939746
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-17 16:00:32 -0300

    chore: rename App.java -> Main.java

--- src/main/java/org/william/eva/Main.java
+++ src/main/java/org/william/eva/Main.java
@@ -3,7 +3,7 @@ package org.william.eva;
 /**
  * Hello world!
  */
-public class App {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
