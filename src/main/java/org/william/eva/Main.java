e0ef4e4d70e5d3d2c339513fcbef412ab20babc3
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-19 15:06:55 -0300

    style: add author information to the source code

--- src/main/java/org/william/eva/Main.java
+++ src/main/java/org/william/eva/Main.java
@@ -2,6 +2,11 @@ package org.william.eva;
 
 import org.william.eva.ui.Frame;
 
+/**
+ * @author William Rios
+ * @author https://github.com/wiirios
+ */
+
 public class Main {
     public static void main(String[] args) {
         new Frame();
