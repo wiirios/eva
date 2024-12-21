f08c33d7d641061defd377edb98b2b8ee2f9fe52
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-20 21:55:19 -0300

    feat: create getters and setters for FileEntity

--- src/main/java/org/william/eva/io/FileEntity.java
+++ src/main/java/org/william/eva/io/FileEntity.java
@@ -1,4 +1,42 @@
 package org.william.eva.io;
 
 public class FileEntity {
+	String name;
+	String extension;
+	Long size;
+	
+	// constructor
+
+	public FileEntity(String name, String extension, Long size){
+		this.name = name;
+		this.extension = extension;
+		this.size = size;
+	}
+	
+	// getters and setters
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getExtension() {
+		return extension;
+	}
+	
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+	
+	public Long getSize() {
+		return size;
+	}
+
+	public void setSize(Long size) {
+		this.size = size;
+	}
+
 }
