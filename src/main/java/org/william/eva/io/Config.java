54f83992ef725cdfc84bdfd08753fde8563c3559
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-23 12:56:17 -0300

    feat: create getTheme function

--- src/main/java/org/william/eva/io/Config.java
+++ src/main/java/org/william/eva/io/Config.java
@@ -1,5 +1,20 @@
 package org.william.eva.io;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Config  {
+	private String propertiesPath = "./src/main/resources/config.properties";
+	
+	public String getTheme() throws IOException {
+		Properties properties = new Properties();
+		FileInputStream fileInputStream = new FileInputStream(propertiesPath);
+		
+		properties.load(fileInputStream);
+		String theme = properties.getProperty("theme");
+		
+		return theme;
+	}
 
 }
