3a653653e5ca6baef87993e07caeea2c103c9572
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-30 14:00:03 -0300

    feat: create message enum

---
+++ src/main/java/org/william/eva/io/Message.java
@@ -0,0 +1,45 @@
+package org.william.eva.io;
+
+import java.awt.Color;
+
+public enum Message {
+	ISOPEN("It is not possible to save a file without first opening one", 1, Color.RED),
+	OPENFILE("Open File", 2, Color.GREEN),
+	CLOSEDFILE("Close File", 3, Color.RED);
+	
+	private String message;
+	private int id;
+	private Color color;
+		
+	Message(String message, int id, Color color) {
+		this.message = message;
+		this.id = id;
+		this.color = color;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	
+}
