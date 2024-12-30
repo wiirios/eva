f993e9277c22e859df066dc43942069fbefc8108
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-30 16:52:05 -0300

    refactor: remove colons from message enum descriptions

--- src/main/java/org/william/eva/io/Message.java
+++ src/main/java/org/william/eva/io/Message.java
@@ -4,9 +4,9 @@ import java.awt.Color;
 
 public enum Message {
 	ISOPEN("It is not possible to save a file without first opening one", 1, Color.RED),
-	OPENFILE("Open File:", 2, Color.GREEN),
-	CLOSEDFILE("Close File:", 3, Color.RED),
-	SAVEFILE("Save File:", 4, Color.BLUE);
+	OPENFILE("Open File", 2, Color.GREEN),
+	CLOSEDFILE("Close File", 3, Color.RED),
+	SAVEFILE("Save File", 4, Color.BLUE);
 	
 	private String message;
 	private int id;
