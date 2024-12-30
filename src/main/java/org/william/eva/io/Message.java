f46da832bcefd5f68d8130059609313ba81a3640
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-30 16:01:11 -0300

    feat: add savefile constant to message enum

--- src/main/java/org/william/eva/io/Message.java
+++ src/main/java/org/william/eva/io/Message.java
@@ -4,13 +4,14 @@ import java.awt.Color;
 
 public enum Message {
 	ISOPEN("It is not possible to save a file without first opening one", 1, Color.RED),
-	OPENFILE("Open File", 2, Color.GREEN),
-	CLOSEDFILE("Close File", 3, Color.RED);
+	OPENFILE("Open File:", 2, Color.GREEN),
+	CLOSEDFILE("Close File:", 3, Color.RED),
+	SAVEFILE("Save File:", 4, Color.BLUE);
 	
 	private String message;
 	private int id;
 	private Color color;
-		
+	
 	Message(String message, int id, Color color) {
 		this.message = message;
 		this.id = id;
@@ -40,6 +41,4 @@ public enum Message {
 	public void setColor(Color color) {
 		this.color = color;
 	}
-	
-	
 }
