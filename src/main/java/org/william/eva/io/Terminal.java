28cc537954ea09e7a1df7fe3c9de1a32054ae512
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-30 16:50:12 -0300

    feat: create logFileAction function in terminal class

--- src/main/java/org/william/eva/io/Terminal.java
+++ src/main/java/org/william/eva/io/Terminal.java
@@ -1,6 +1,20 @@
 package org.william.eva.io;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Terminal {
+	private LocalDateTime dateLocal;
 	
-	
+	public String logFileAction(String message, String fileName) {
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		dateLocal = LocalDateTime.now(Clock.systemDefaultZone());
+		String customPattern = dateLocal.format(DateTimeFormatter.ofPattern("H:m:s"));
+		
+		stringBuilder.append(customPattern + ": " + message + ": " + fileName);
+				
+		return String.valueOf(stringBuilder);
+	}
 }
