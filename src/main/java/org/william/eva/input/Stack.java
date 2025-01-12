685f01d6bc72a556125973162eca77ac925b95c1
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-01-11 23:05:44 -0300

    feat: implement core operations

--- src/main/java/org/william/eva/input/Stack.java
+++ src/main/java/org/william/eva/input/Stack.java
@@ -1,5 +1,50 @@
 package org.william.eva.input;
 
-public class Stack {
+import java.util.ArrayList;
 
+public class Stack<T> {
+	private ArrayList<T> stack = new ArrayList<T>();
+	private int stackLength = 0;
+	private static boolean isEmpty = true;
+	
+	public Stack() {}
+
+	public void push(T value) {
+		stack.add(value);
+		isEmpty = false;
+		
+		if (empty() == false) stackLength++;
+	}	
+	
+	public void pop() {
+		if (empty() == false) {
+			stack.remove(stackLength-1);
+			
+			if (stackLength == 1) {
+				isEmpty = true;
+			}
+			stackLength--;
+		}
+	}
+	
+	public void getAll() {		
+		for (T i:stack) {
+			System.out.println(i);
+		}
+	}
+	
+	public T peek() {
+		if (empty() == false) {
+			return stack.get(stackLength-1);
+		}
+		return null;
+	}
+	
+	public static boolean empty() {
+		return isEmpty;
+	}
+	
+	public int length() {
+		return stackLength;
+	}
 }
