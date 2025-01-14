e78c1897e88dcef9dc4f3aae5d83527146eb33be
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-01-14 12:08:27 -0300

    feat: create undo class constructor

--- src/main/java/org/william/eva/input/Undo.java
+++ src/main/java/org/william/eva/input/Undo.java
@@ -1,5 +1,19 @@
 package org.william.eva.input;
 
+import javax.swing.JTextPane;
+
 public class Undo {
-	public Undo() {}
+	private JTextPane textPane;
+	
+	public JTextPane getTextPane() {
+		return textPane;
+	}
+
+	public void setTextPane(JTextPane textPane) {
+		this.textPane = textPane;
+	}
+	
+	public Undo(JTextPane textPane) {
+		this.textPane = textPane;
+	}
 }
