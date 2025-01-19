fc51a7edd24779b9b8c0889c8206af02eb93f385
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-01-19 17:57:11 -0300

    feat: add attributes to the run class

--- src/main/java/org/william/eva/runner/Run.java
+++ src/main/java/org/william/eva/runner/Run.java
@@ -1,5 +1,12 @@
 package org.william.eva.runner;
 
+import java.nio.file.Path;
+
 public class Run {
+	private String name;
+	private String extension;
+	private Path path;
+	private String compilerPath;
+	
 	public Run() {}
 }
