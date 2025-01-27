8de08e9646f531f91017473cee0df4753d5ec9b0
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-01-27 11:33:40 -0300

    feat: create compiler constructor

--- src/main/java/org/william/eva/runtime/Compiler.java
+++ src/main/java/org/william/eva/runtime/Compiler.java
@@ -1,5 +1,16 @@
 package org.william.eva.runtime;
 
+import java.nio.file.Path;
+
 public class Compiler {
+	private String name;
+	private String extension;
+	private Path path;
+	
+	public Compiler(String name, String extension, Path path) {
+		this.name = name;
+		this.extension = extension;
+		this.path = path;
+	}	
 
 }
