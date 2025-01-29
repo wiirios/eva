ac49b1fd2e83f21992edc10509ef296f1db71e5d
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-01-29 14:45:56 -0300

    feat: finalize run method

--- src/main/java/org/william/eva/runtime/Runner.java
+++ src/main/java/org/william/eva/runtime/Runner.java
@@ -6,30 +6,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 
-public class Runner {
+public class Runner implements Runnable {
 	private String name;
 	private String extension;
 	private Path path;
 	
-	private StringBuilder stringBuilder = new StringBuilder();
+	private StringBuilder stringBuilder;
 	private String line = null;
-	private Thread thread;
 	
 	public Runner(String name, String extension, Path path) {
 		this.name = name;
 		this.extension = extension;
 		this.path = path;
-	}		
-	
-	public void runnable() throws IOException {	
-		//thread = new Thread(new Runner());
-		thread.start();
 	}
-	
-	public boolean isRun() {
-		return thread.isAlive();
+
+	@Override
+	public void run() {
+		stringBuilder = new StringBuilder();
+		String pathString = path.toString();
+		File dir = new File(pathString.substring(0, pathString.lastIndexOf("\\") + 1));
+		
+		switch (extension) {
+		case ".java":
+			String[] javaRunRegedit = new String[] {"cmd.exe", "/c", "java " + name};
+			
+			/* i will still create an option in the preferences to be able to customize the path to the evnp of the chosen language */
+			// String[] envy = new String[] {"JAVA_HOME", "C:\\Program Files\\Java\\"};
+			// String [] customEnvp;
+								
+			try {
+				Runtime runtime = Runtime.getRuntime();
+				Process process = runtime.exec(javaRunRegedit, null, dir);
+				
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				while((line = bufferedReader.readLine()) != null) {
+					stringBuilder.append(line);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		default:
+			break;
+		}
 	}
-	
+
 	public void resetOutputState() {
 		stringBuilder.setLength(0);
 		line = null;
