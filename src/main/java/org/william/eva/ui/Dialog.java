2857d09bb53bbfac9747948c99dc60a41200d6ca
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-06-08 11:00:52 -0300

    fix(ui): save button height

--- src/main/java/org/william/eva/ui/Dialog.java
+++ src/main/java/org/william/eva/ui/Dialog.java
@@ -12,6 +12,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JList;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Locale;
 
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
@@ -21,6 +24,12 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import org.william.eva.annotation.Incomplete;
+import org.william.eva.io.Config;
+import org.william.eva.util.Resources;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
 import javax.swing.JButton;
 import java.awt.Component;
 import javax.swing.Box;
@@ -30,10 +39,13 @@ public class Dialog extends JDialog {
     private static final int HEIGHT = 300;
     private static final int WIDTH = HEIGHT * 16 / 9;
 
-    private String[] languages = new String[]{"EN", "PT"};
-    private String[] themes = new String[]{"Dark", "Light"};
-    private String[] charset = new String[]{"ISO-8859-1", "US-ASCII", "UTF-8", "UTF-16BE", "UTF-16LE", "UTF-16", "UTF-32BE", "UTF-32LE", "UTF-32"};
+    private String[] languages = {"EN", "PT"};
+    private String[] themes = {"Dark", "Light"};
+    private String[] charset = {"ISO-8859-1", "US-ASCII", "UTF-8", "UTF-16BE", "UTF-16LE", "UTF-16", "UTF-32BE", "UTF-32LE", "UTF-32"};
 
+    private Resources resources;
+    private Config config;
+    
     private CardLayout cardLayout;
     private JPanel container;
 
@@ -44,14 +56,16 @@ public class Dialog extends JDialog {
 
     @Incomplete
     private void initialize() {
+    	resources = new Resources(Locale.getDefault().getLanguage(), Locale.getDefault().getCountry());    
+    	
         this.setSize(WIDTH, HEIGHT);
         this.setModalityType(DEFAULT_MODALITY_TYPE);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
-        this.setTitle("Preferences");
+        this.setTitle(resources.getText("preferences"));
         getContentPane().setLayout(new BorderLayout(0, 0));
-        
-        JList<String> list = new JList<>(new String[]{"General", "Language"});
+          
+        JList<String> list = new JList<>(new String[]{resources.getText("general"), resources.getText("language")});
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         list.setSelectedIndex(0);
         getContentPane().add(new JScrollPane(list), BorderLayout.WEST);
@@ -64,11 +78,12 @@ public class Dialog extends JDialog {
         panelGeneral.setLayout(new BoxLayout(panelGeneral, BoxLayout.Y_AXIS));
 
         JPanel themePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        themePanel.add(new JLabel("Theme"));
+        themePanel.add(new JLabel(resources.getText("theme")));
         
         Dimension size = new Dimension(WIDTH, 30);
         
-        themePanel.add(new JComboBox<>(themes));
+        JComboBox<String> jComboBoxThemes = new JComboBox<>(themes);
+        themePanel.add(jComboBoxThemes);
         themePanel.setPreferredSize(size);
         themePanel.setMaximumSize(themePanel.getPreferredSize()); 
         themePanel.setMinimumSize(themePanel.getPreferredSize());
@@ -79,52 +94,82 @@ public class Dialog extends JDialog {
         charsetPanel.setPreferredSize(size);
         charsetPanel.setMaximumSize(charsetPanel.getPreferredSize()); 
         charsetPanel.setMinimumSize(charsetPanel.getPreferredSize());
-        charsetPanel.add(new JComboBox<>(charset));
         
+        JComboBox<String> charsetBox = new JComboBox<>(charset);
+        charsetPanel.add(charsetBox);
         panelGeneral.add(charsetPanel);
         
         JCheckBox jCheckBox = new JCheckBox();
+        jCheckBox.setSelected(true);     
+           
         JPanel saveIntervalPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        saveIntervalPanel.add(new JLabel("Auto save (default: on)"));
+        saveIntervalPanel.add(new JLabel(resources.getText("autosave")));
         saveIntervalPanel.setPreferredSize(size);
         saveIntervalPanel.setMaximumSize(saveIntervalPanel.getPreferredSize());
         saveIntervalPanel.setMinimumSize(saveIntervalPanel.getPreferredSize());
         saveIntervalPanel.add(jCheckBox);
         
         panelGeneral.add(saveIntervalPanel);
+        
+        JCheckBox jCheckBoxSystemTheme = new JCheckBox();
+        jCheckBoxSystemTheme.setSelected(true);
+        
+        JPanel getThemeSystemPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        getThemeSystemPanel.add(new JLabel(resources.getText("getthemefromsystem")));
+        getThemeSystemPanel.setPreferredSize(size);
+        getThemeSystemPanel.setMaximumSize(getThemeSystemPanel.getPreferredSize());
+        getThemeSystemPanel.setMinimumSize(getThemeSystemPanel.getPreferredSize());
+        getThemeSystemPanel.add(jCheckBoxSystemTheme);
+        
+        panelGeneral.add(getThemeSystemPanel);
 
         JPanel intervalPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        intervalPanel.add(new JLabel("Editor save interval (in minutes)"));
+        intervalPanel.add(new JLabel(resources.getText("editorsave")));
         intervalPanel.setPreferredSize(size);
         intervalPanel.setMaximumSize(intervalPanel.getPreferredSize()); 
         intervalPanel.setMinimumSize(intervalPanel.getPreferredSize());
-        intervalPanel.add(new JTextField("10", 10));
         
+        JTextField saveIntervalField = new JTextField("10", 10);
+        intervalPanel.add(saveIntervalField);        
         panelGeneral.add(intervalPanel);
         
-        Component verticalStrut = Box.createVerticalStrut((int) (themePanel.getPreferredSize().height * 3.5));
-        panelGeneral.add(verticalStrut);
-        
         JPanel savePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        savePanel.add(new JButton("Save"));
+        JButton jButtonSave = new JButton(resources.getText("save"));
+        savePanel.add(jButtonSave);
         savePanel.setPreferredSize(size);
         savePanel.setMaximumSize(savePanel.getPreferredSize()); 
-        savePanel.setMinimumSize(savePanel.getPreferredSize());            
+        savePanel.setMinimumSize(savePanel.getPreferredSize());
         
         panelGeneral.add(savePanel);
 
         JPanel panelLanguage = new JPanel();
-        panelLanguage.add(new JLabel("Language"));
+        panelLanguage.add(new JLabel(resources.getText("language")));
         panelLanguage.add(new JComboBox<>(languages));
 
-        container.add(panelGeneral, "General");
-        container.add(panelLanguage, "Language");
+        container.add(panelGeneral, resources.getText("general"));
+        container.add(panelLanguage, resources.getText("language"));
 
         list.addListSelectionListener(new ListSelectionListener() {
             public void valueChanged(ListSelectionEvent e) {
                 String selected = list.getSelectedValue();
                 cardLayout.show(container, selected);
             }
+        });
+        
+        jButtonSave.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//System.out.println(jComboBoxThemes.getSelectedItem());
+				//System.out.println(charsetBox.getSelectedItem());
+				//System.out.println(jCheckBox.isSelected()); // auto save
+				//System.out.println(jCheckBoxSystemTheme.isSelected()); // get system theme
+				//System.out.println(saveIntervalField.getText());
+
+				//jComboBoxThemes.getSelectedItem();
+				
+			}
+        	
         });
 
         this.setVisible(true);
